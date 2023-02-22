7dd35b7c5d7004ddfcc70a8d11e0fae4587f72fa
Mats-Kalleberg <70912119+Mats-Kalleberg@users.noreply.github.com>
2023-02-22 14:48:09 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Uninstall the telemetrytv snap package
+sudo snap remove telemetrytv
+
+# Set the URL to open in the kiosk window
+URL="https://v.getcloudcast.com"
+
+# Create the kiosk script
+echo '#!/bin/bash
+while true; do
+    chromium-browser --noerrdialogs --disable-infobars --kiosk '"$URL"'
+done' | sudo tee /home/infoskjerm/kiosk.sh >/dev/null
+
+# Set the correct permissions for the kiosk script
+sudo chmod +x /home/infoskjerm/kiosk.sh
+
+# Create the kiosk desktop file
+echo '[Desktop Entry]
+Type=Application
+Name=Kiosk
+Exec=/home/infoskjerm/kiosk.sh
+X-GNOME-Autostart-enabled=true' | sudo tee /etc/xdg/autostart/kiosk.desktop >/dev/null
