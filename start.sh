4561926d909717edc0dcb70ad613f7cd5b99ed09
Mats-Kalleberg <70912119+Mats-Kalleberg@users.noreply.github.com>
2023-05-24 10:56:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ Exec=/home/infoskjerm/kiosk.sh
 X-GNOME-Autostart-enabled=true' | sudo tee /etc/xdg/autostart/kiosk.desktop >/dev/null
 
 # Add the cronjob
-sudo bash -c 'crontab -l | sed "$!b" ; echo "30 7 * * 1-5 killall chrome >/dev/null 2>&1" | crontab -'
+sudo bash -c 'echo "30 7 * * 1-5 killall chrome >/dev/null 2>&1" >> /etc/crontab'
 
 # Display the modified crontab file
 sudo crontab -l
